e9c6df7eff8a3de9a0a04632e40c8832719cd93a
prateek041 <prateeksingh9741@gmail.com>
2023-05-09 00:22:45 +0530

    Added: third exercise in second section
    Signed-off-by: prateek041 <prateeksingh9741@gmail.com>

--- ex2/ex2-3.go
+++ ex2/ex2-3.go
@@ -5,8 +5,10 @@ import (
 	"fmt"
 	"log"
 
+	apiv1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
+	appsv1 "k8s.io/client-go/kubernetes/typed/apps/v1"
 	"k8s.io/client-go/tools/clientcmd"
 )
 
@@ -23,14 +25,32 @@ func getPodsInCluster() {
 		log.Fatal("Error in creating the client")
 	}
 
-	podClient := client.CoreV1().Pods("kube-system")
-	pods, err := podClient.List(context.Background(), metav1.ListOptions{})
+	name := "nginx-deployment"
+	deploymentClient := client.AppsV1().Deployments(apiv1.NamespaceDefault)
+	deleteDeployment(deploymentClient, name)
+}
+
+func deleteDeployment(deploymentClient appsv1.DeploymentInterface, name string) {
+	err := deploymentClient.Delete(context.Background(), name, metav1.DeleteOptions{})
 	if err != nil {
-		log.Fatal("Error in getting the podlist", err)
+		log.Fatal("Error in deleting the deployment", err)
+	} else {
+		fmt.Println("Succesfully deleted the deployment nginx-deployment")
+	}
+}
+
+// If you want to list all the deployments in the default namespace.
+func getDeploymentList(deploymentClient appsv1.DeploymentInterface) {
+	deploymentList, err := deploymentClient.List(context.Background(), metav1.ListOptions{})
+	if err != nil {
+		log.Fatal("Error in getting the deploymente in the deafult namespace", err)
 	}
 
-	fmt.Println("Pods in the kube-system namespace kube-system")
-	for _, pod := range pods.Items {
-		fmt.Println(pod.Name)
+	if len(deploymentList.Items) == 0 {
+		log.Fatal("No deployments in the default namespace")
+	}
+
+	for _, deployment := range deploymentList.Items {
+		fmt.Println("Deployment ", deployment.Name)
 	}
 }
